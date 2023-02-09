912205389bf309a4cd0328c132445dcbbb4feeb7
BryantVaughn <bryant.f.vaughn@gmail.com>
2023-02-08 23:24:29 -0700

    Add users class and initial get/post endpoints

---
+++ src/main/java/com/bryantvaughn/rest/webservices/restfulwebservices/user/UserDaoService.java
@@ -0,0 +1,35 @@
+package com.bryantvaughn.rest.webservices.restfulwebservices.user;
+
+import org.springframework.stereotype.Component;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.Predicate;
+
+@Component
+public class UserDaoService {
+    private static final List<User> users = new ArrayList<>();
+    private static int usersCount = 0;
+
+    static {
+        users.add(new User(++usersCount, "Bryant", LocalDate.of(1990, 7, 18)));
+        users.add(new User(++usersCount, "Lynn", LocalDate.of(1991, 4, 9)));
+        users.add(new User(++usersCount, "Ginger", LocalDate.of(2021, 3, 12)));
+    }
+
+    public List<User> findAll() {
+        return users;
+    }
+
+    public User findOne(int id) {
+        Predicate<? super User> predicate = user -> user.getId() == id;
+        return users.stream().filter(predicate).findFirst().orElse(null);
+    }
+
+    public User save(User user) {
+        user.setId(++usersCount);
+        users.add(user);
+        return user;
+    }
+}
---
+++ src/main/java/com/bryantvaughn/rest/webservices/restfulwebservices/user/UserResource.java
@@ -0,0 +1,38 @@
+package com.bryantvaughn.rest.webservices.restfulwebservices.user;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.net.URI;
+import java.util.List;
+
+@RestController
+public class UserResource {
+    private final UserDaoService service;
+
+    public UserResource(UserDaoService service) {
+        this.service = service;
+    }
+
+    @GetMapping("/users")
+    public List<User> getAllUsers() {
+        return service.findAll();
+    }
+
+    @GetMapping("/users/{id}")
+    public User getUserById(@PathVariable int id) {
+        return service.findOne(id);
+    }
+
+    @PostMapping(path = "/users", consumes = "application/json")
+    public ResponseEntity<User> createUser(@RequestBody User user) {
+        User newUser = service.save(user);
+        URI location = ServletUriComponentsBuilder
+                .fromCurrentRequest()
+                .path("/{id}")
+                .buildAndExpand(newUser.getId())
+                .toUri();
+        return ResponseEntity.created(location).build();
+    }
+}
